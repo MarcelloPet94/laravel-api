17a660d888e93ad18c7f58a4480d67aff829573d
marcello <marcellopettenuzzo2@gmail.com>
2022-03-22 14:35:06 +0100

    first commit

---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,44 @@
+<div class="align_controller">
+@include('layouts.admin')
+    <div class="container">
+    <h1>Crea un nuovo post</h1>
+        <form action="{{ route('admin.posts.store') }}" method="POST">
+            @csrf
+
+            <div class="form-group">
+                <input type="text" class="form-control" id="title" name="title" placeholder="Titolo del post">
+            </div>
+            <div class="form-group">
+                <textarea class="form-control" id="content" name="content"
+                    placeholder="Descrizione del prodotto"></textarea>
+            </div>
+            <div class="space_btn_area">
+                 <a href="{{ route('admin.posts.index')}}" class="btnStyl goback">Go back</a>
+                <button type="submit" class="btn_funct save_edit_btn">Edit</button>
+            </div>  
+
+            @foreach ($tags as $tag)
+                
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="{{$tag->id}}" id="{{$tag->slug}}" name="tags[]">
+                <label class="form-check-label" for="{{$tag->slug}}">
+                    {{$tag->name}}
+                </label>
+                </div>
+
+            @endforeach   
+
+        </form>
+
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                    
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+    </div>  
+</div>    
---
+++ resources/views/admin/posts/show.blade.php
@@ -0,0 +1,39 @@
+<div class="align_controller">
+@include('layouts.admin')
+    <div class="container">
+        <div class="space_btn_area">
+            <h2>Post ID: {{$post->id}}</h2>
+        </div>         
+        <div class="riga ind">
+            <ul>
+                <li>ID</li>
+                <li>Title</li>
+                <li>Contenuto</li>
+                <li>Slug</li>
+                <li>Tags</li>
+                <li>Operation</li>
+            </ul>
+        </div>
+            <div class="riga">
+                <ul>
+                    <li>{{$post->id}}</li>
+                    <li>{{$post->title}}</li>
+                    <li>{{$post->content}}</li>
+                    <li>{{$post->slug}}</li>
+                    <li>
+                        @foreach ($post->$tags as $tag)
+                            {{ $tag->name }}
+                        @endforeach                        
+                    </li>
+                    <li>
+                        <a  href="{{ route('admin.posts.edit', $post->id) }}">Edit</a>
+                    </li>
+                </ul>    
+            </div> 
+            <div class="space_btn_area">
+                <a href="{{ route('admin.posts.index')}}" class="btnStyl goback">Go back</a>
+            </div>            
+    </div>  
+</div>    
+
+         
